390f456bd2f03bc2fe09b07ffd51ecbe0e19b004
DVP <dvp@TechAcademy01s-MacBook-Pro.local>
2024-02-18 09:28:11 +0300

    Initial Commit

---
+++ calc/calcApp.swift
@@ -0,0 +1,17 @@
+//
+//  calcApp.swift
+//  calc
+//
+//  Created by DVP on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct calcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
